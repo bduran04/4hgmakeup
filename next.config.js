63c26a4cf3f34c19ac7f63829f42d89a79be2dfe
Belle Duran <bduran04@gmail.com>
2025-06-24 19:28:28 -0500

    updated next.config.js file to allow images through google drive

--- next.config.js
+++ next.config.js
@@ -1,15 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          pathname: "**", 
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;  
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**", 
+      },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "docs.google.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "googleusercontent.com",
+        pathname: "**",
+      },
+      // Optional: Add other common free image hosting services
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "imgur.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
